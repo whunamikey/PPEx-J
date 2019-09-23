3886ce100ec26f4e92facb2602009f2017884e45
125859320@qq.com <125859320@qq.com>
2019-09-23 21:16:07 +0800

    添加Client启动类

---
+++ src/main/java/Client.java
@@ -0,0 +1,8 @@
+import ppex.socket.udp.UdpClient;
+
+public class Client {
+    public static void main(String[] args){
+        UdpClient client = new UdpClient();
+        client.startClient();
+    }
+}
